83cdfbf6768ffdaa48978c6091c275c2af2bd4f6
Mahan <mahan.vahid@yahoo.com>
2021-10-31 18:29:53 +0100

    Added the following in the dropdown
    New GD-File
        *Creates an empty file (Done by browsing)
    New Arduino-File
        *Ability to create a new folder on the top-right side of the file picker
        *Creates a new file using a template
                 *( sets up the setup() function and loop() function following a template file in the source folder)

--- project/src/ui/code_editor/MainWindow.gd
+++ project/src/ui/code_editor/MainWindow.gd
@@ -6,6 +6,11 @@ onready var fileDialog: FileDialog = $FileDialog
 onready var textEditor: TextEdit = $TextEditor
 onready var _path: String = ""
 
+#SAVES CURRENT STATE OF filedialog operation
+#Can have the following values:
+# OPEN  NEWFILE  SAVE NEWPROJ
+onready var fileDialogOperation: String = ""
+
 onready var fileLoader = load("res://src/ui/file_dialog/FileLoader.gd").new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +23,8 @@ func _init_dropdown():
 	dropdown_btn.get_popup().connect("id_pressed",self, "_on_item_pressed")
 	dropdown_btn.get_popup().add_item("Open File")
 	dropdown_btn.get_popup().add_item("Save File")
+	dropdown_btn.get_popup().add_item("New GD-File")
+	dropdown_btn.get_popup().add_item("New Arduino-File")
 	dropdown_btn.get_popup().add_item("Close")
 
 #Initializes the texteditor settings
@@ -42,26 +49,56 @@ func enableEditor() -> void:
 func _on_item_pressed(id):
 	var name = dropdown_btn.get_popup().get_item_text(id)
 	if name == "Open File":
+		fileDialogOperation = "OPEN"
 		fileDialog.popup() # Opens file dialog for file selection
 	elif name == "Save File":
-		_save_file()
+		_save_file(_path)
+	elif name == "New GD-File":
+		_new_file()
+	elif name == "New Arduino-File":
+		_new_proj()
 	elif name == "Close":
 		_on_close()
 
+	
 # Function to collect the path of a selected file and send it to the editor
 func _on_FileDialog_file_selected(path):
 	#Save global path for quick save
 	_path = path
-	#Load text from file
-	var content = fileLoader.loadFile(path)
-	#Paste text into texteditor
-	textEditor.text = (content)
-
+	
+	if(fileDialogOperation == "OPEN"):
+		print(path)
+		fileDialogOperation = ""
+		#Load text from file
+		var content = fileLoader.loadFile(path)
+		#Paste text into texteditor
+		textEditor.text = (content)
+	elif(fileDialogOperation == "NEWFILE" || fileDialogOperation == "NEWPROJ"):
+		
+		fileDialog.mode = fileDialog.MODE_OPEN_FILE	#Change mode back to open file	
+		_save_file(path)
+		if(fileDialogOperation == "NEWFILE"):
+			textEditor.text = ""
+		if(fileDialogOperation == "NEWPROJ"):
+			textEditor.text = fileLoader.loadFile("res://NewArduinoTemplate.txt")
+		fileDialogOperation = ""
+		
+		
 
 # Function save a file
-func _save_file():
+func _save_file(path):
 	#save text into texteditor
-	fileLoader.saveFile(_path,textEditor.text)
+	fileLoader.saveFile(path,textEditor.text)
+
+#Function to create a new file
+func _new_file():
+	fileDialogOperation = "NEWFILE"
+	fileDialog.mode = fileDialog.MODE_SAVE_FILE	#Change mode to open dir
+	fileDialog.popup()	#Get path for new file
 	
-
-
+#Function to create a new file
+func _new_proj():
+	fileDialogOperation = "NEWPROJ"
+	fileDialog.mode = fileDialog.MODE_SAVE_FILE	#Change mode to open dir
+	fileDialog.popup()	#Get path for new file
+	
